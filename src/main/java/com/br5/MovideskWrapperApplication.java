d7f983b2f644b4ce10d25b8b2ca8384b0dd361e8
josefinco <josefinco_@hotmail.com>
2022-01-04 22:26:42 -0300

    Initial commit

---
+++ src/main/java/com/br5/MovideskWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.br5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovideskWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovideskWrapperApplication.class, args);
+	}
+
+}
